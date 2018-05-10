bfbdec00ae23152eca1464e6da33e346ce1921bb
Henrique D. Garcia <henriquedgarcia@gmail.com>
2018-05-10 16:49:45 -0300

    mexi na formatação da saída.

--- lzwcodec.h
+++ lzwcodec.h
@@ -20,12 +20,12 @@ public:
   int Decoder(string nameIn, string nameOut);
 
 private:
-  fstream m_inputFile;
-  fstream m_outputFile;
-  ui m_alphabetSize = 256;
-  deque<deque<uc>> m_dictionary;
-  deque<ui> m_output;
-  deque<deque<uc>> m_outputDecoder;
+fstream m_inputFile;
+fstream m_outputFile;
+ui m_alphabetSize = 256;
+deque<deque<uc>> m_dictionary;
+deque<ui> m_output;
+deque<deque<uc>> m_outputDecoder;
 
   void GravaSaidaDecoder(void);
   void OpenFiles(string nameIn, string nameOut);
